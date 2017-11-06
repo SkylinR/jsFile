1283613fad62c74150f2b4d75cfade188d871c9c
realitymac <realitymac@Mac-mini-realitymac.local>
2017-11-06 13:55:43 +0100

    test

--- map.js
+++ map.js
@@ -103,6 +103,8 @@ var colors = {
 showResult = function(searchResult) {
   var circleStroke = true;
   if(map.getZoom() > 10) {
+    console.log("TEST");
+
     circleStroke = false;
     //cleaning markers
     markers.forEach(marker => map.removeLayer(marker))
